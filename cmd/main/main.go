efa8a112a6e5bd81940f9798e550c18b2dfaae24
Ryan Toh <ryedralisk@gmail.com>
2023-07-14 18:52:31 +0800

    feat: route support for /hotel and /flight

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,14 +1,45 @@
 package main
 
 import "fmt"
+import "net/http"
 
 import "github.com/Rye123/csit-swe-challenge-2023/internal/db"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	flights := db.Flights("2023-03-09", "2023-04-01", "London")
-	hotels := db.Hotels("2023-03-11", "2023-03-29", "London")
+	router := gin.Default()
+	router.GET("/flight", getFlights)
+	router.GET("/hotel", getHotels)
 
-	fmt.Println(flights)
+	router.Run(":8080")
+	fmt.Println("Server running on http://localhost:8080")
+}
+
+// Serves URL: /flight?departureDate=2023-12-10&returnDate=2023-12-16&destination=Frankfurt
+func getFlights(c *gin.Context) {
+	departureDate := c.Query("departureDate")
+	returnDate := c.Query("returnDate")
+	destination := c.Query("destination")
+
+	flights, err := db.Flights(departureDate, returnDate, destination, -1)
+	if err != nil || len(flights) == 0 {
+		c.JSON(http.StatusNotFound, nil)
+	} else {
+		c.JSON(http.StatusOK, flights)
+	}
+}
+
+// Serves URL: /hotel?checkInDate=2023-12-10&checkOutDate=2023-12-16&destination=Frankfurt
+func getHotels(c *gin.Context) {
+	checkInDate := c.Query("checkInDate")
+	checkOutDate := c.Query("checkOutDate")
+	destination := c.Query("destination")
+
+	hotels, err := db.Hotels(checkInDate, checkOutDate, destination, -1)
 	fmt.Println(hotels)
-	
+	if err != nil || len(hotels) == 0 {
+		c.JSON(http.StatusNotFound, nil)
+	} else {
+		c.JSON(http.StatusOK, hotels)
+	}
 }
